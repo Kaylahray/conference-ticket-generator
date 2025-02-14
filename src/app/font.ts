7db5f38cb1f7ee57b7ea22bc5527653cb69a7366
Kaylahray <chiomachristopher91@gmail.com>
2025-02-14 20:05:34 +0100

    deployment error fix

--- src/app/font.ts
+++ src/app/font.ts
@@ -1,14 +1,11 @@
 // app/fonts.ts
 import { Roboto, Road_Rage } from "next/font/google";
-import localFont from "next/font/local";
 
 export const roboto = Roboto({
   subsets: ["latin"],
   weight: ["400", "700"],
   variable: "--font-roboto",
 });
-
-
 
 export const roadRage = Road_Rage({
   subsets: ["latin"],
