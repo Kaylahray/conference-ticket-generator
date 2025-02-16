18e79dd4dc29217e3243ff4c0a05c1ef6015f64b
Kaylahray <chiomachristopher91@gmail.com>
2025-02-16 04:01:11 +0100

    error handling for fullname

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,7 +1,13 @@
 import { z } from "zod";
 
 export const schema = z.object({
-  fullName: z.string().regex(/^[a-zA-Z\s]+$/, "Name should only contain alphabetic characters and spaces"),
+  fullName: z
+    .string()
+    .min(1, "Name is required")
+    .regex(
+      /^[a-zA-Z\s]+$/,
+      "Name should only contain alphabetic characters and spaces"
+    ),
   email: z.string().min(1, "Email is required").email("Invalid email address"),
   avatarUrl: z.string(),
   request: z.string().optional(),
